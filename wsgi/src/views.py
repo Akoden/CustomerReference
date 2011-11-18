bc8906838bd1f470fd430cb25a6a5896c60b30cf
ROTOUDJIMAYE Theophile <rotoudjimaye.theo@gmail.com>
2011-11-18 23:36:30 +0100

    test change

--- wsgi/src/views.py
+++ wsgi/src/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render_to_response('index.html')
 
+
 @login_required
 def secure(request):
     return render_to_response('secure.html')
