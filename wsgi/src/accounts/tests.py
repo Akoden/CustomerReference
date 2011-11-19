2835cca685b4c046605d00418d424a9a35aab0f9
ROTOUDJIMAYE Theophile <rotoudjimaye.theo@gmail.com>
2011-11-19 21:45:08 +0100

    add selenium test + integration to saucelabs

---
+++ wsgi/src/accounts/tests.py
@@ -0,0 +1,72 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import unittest, time, re
+
+
+BROWSER_CAPABILITIES = {
+    'version' : '8',
+    'name' : ''
+}
+
+SAUCELABS_EXECUTOR = "http://rotoudjimaye:02cefab9-c500-4f05-a761-00fc441f3187@ondemand.saucelabs.com:80/wd/hub"
+BASE_URL = "http://csr-theo.rhcloud.com"
+
+class TestSeleniumWebDriver(unittest.TestCase):
+    def setUp(self):
+        capabilities = webdriver.DesiredCapabilities.CHROME
+        capabilities.update(BROWSER_CAPABILITIES)
+        # self.driver = webdriver.Firefox()
+        self.driver = webdriver.Remote(desired_capabilities=capabilities, command_executor=SAUCELABS_EXECUTOR)
+        self.driver.implicitly_wait(30)
+        # self.base_url = "http://csr-theo.rhcloud.com/"
+        self.verificationErrors = []
+    
+    def get(self, url):
+        return "".join([BASE_URL, str(url)])
+
+    def testLogin(self):
+        driver = self.driver
+        self.get("/accounts/login/")
+        driver.find_element_by_name("j_username").clear()
+        # driver.find_element_by_name("j_username").send_keys("abc")
+        # driver.find_element_by_name("j_password").clear()
+        # driver.find_element_by_name("j_password").send_keys("ysvf")
+        # driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e: return False
+        return True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+class TestRemote(unittest.TestCase):
+    def setUp(self):
+        self.verificationErrors = []
+        self.selenium = selenium("localhost", 4444, "*chrome", "http://127.0.0.1:8000/")
+        self.selenium.start()
+    
+    def test_remote(self):
+        sel = self.selenium
+        sel.open("/")
+        sel.click("link=secure page")
+        sel.wait_for_page_to_load("30000")
+        sel.type("name=j_username", "abc")
+        sel.type("name=j_password", "ysvf")
+        sel.click("css=input[type=\"submit\"]")
+        sel.wait_for_page_to_load("30000")
+    
+    def tearDown(self):
+        self.selenium.stop()
+        self.assertEqual([], self.verificationErrors)
+
+def suite():
+    return unittest.TestLoader().loadTestsFromTestCase(TestSeleniumWebDriver)
+
+if __name__ == "__main__":
+    unittest.main()
+
--- wsgi/src/settings.py
+++ wsgi/src/settings.py
@@ -119,6 +119,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'admin',
+    'accounts',
     'csr',
     # 'django_jenkins',
     # Uncomment the next line to enable the admin:
