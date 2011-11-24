1538d4e9dec84d498c18f92e080004676ce485a7
ROTOUDJIMAYE Theophile <rotoudjimaye.theo@gmail.com>
2011-11-24 08:36:28 +0100

    syntax fix

--- wsgi/src/csr/utils.py
+++ wsgi/src/csr/utils.py
@@ -2,7 +2,7 @@
 
 def render_ext(view_id):
     def decorator(target):
-        def decorator_logic(*args, *kwargs):
+        def decorator_logic(*args, **kwargs):
 	    resultMap = target(args, kwargs)
 	    resultMap['view_id'] = view_id
 	    return resultMap
