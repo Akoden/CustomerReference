6ea5446a6489a82ad3751fc3ae60ab72a33bcc0b
ROTOUDJIMAYE Theophile <rotoudjimaye.theo@gmail.com>
2011-11-24 08:45:33 +0100

    require simplejson on openshift

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='CustomerReference',
       author='Akoden GmbH',
       author_email='theo@akoden.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Django>=1.3'],
+      install_requires=['Django>=1.3', 'simplejson>=2.2.1'],
      )
