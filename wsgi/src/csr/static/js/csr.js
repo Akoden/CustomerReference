9167ac4a8f2679c113acf98fcd8255ffa55d6cb1
ROTOUDJIMAYE Theophile <rotoudjimaye.theo@gmail.com>
2011-11-25 23:37:49 +0100

    fix login page

--- wsgi/src/csr/static/js/csr.js
+++ wsgi/src/csr/static/js/csr.js
@@ -1,37 +1,48 @@
 
 $(document).ready(function(){
 
-        var $loginDialog = $("#content-inner").append("<div>").find("div").dialog("hide");
+        
+    var $loginDialog;
+    
+    $("#login-status").on('click', function(event){
+	$.get("/accounts/login/", function(result){
 
-	$("#login-status").on('click', function(event){
-	   $.get("/accounts/login/", function(result){
-	        
-		var $form = $(result).find("form");
-		$loginDialog.clear().append($form).dialog('show');
+
+	    if($loginDialog != null){
+		$loginDialog.dialog('close');
+		$loginDialog = null;
+	    }
+
+	    $loginDialog = $("#content-inner").append("<div>").find("div").dialog();
+	    
+	    var $form = $(result).find("form");
+	    $loginDialog.append($form);
+	    
+	    $form.on('submit', function(evt){
+
+		evt.preventDefault();
 		
-		$form.on('submit', function(evt){
-
-		    evt.preventDefault();
-		    
-		    var url = '/accounts/login/.json';
-		    var fieldsData = $form.serialize();
-		    var callback =  function(json, status) {
-		        if( json['result'] == 'success' ){
-			    $("#login-status").text(json['username']);
-			    $("#logout-link").css({'visibility':'visible'});
-			    $loginDialog.dialog('hide');
-			}
-		    };
-
-		    $.ajax({
-  			url: url,
-  			dataType: 'json',
-  			data: fieldsData,
-  			success: callback,
-			type: 'POST'
-		    });
-              });
-	   });
-	   event.preventDefault();
+		var url = '/accounts/login/.json';
+		var fieldsData = $form.serialize();
+		var callback =  function(json, status) {
+		    if( json['result'] == 'success' ){
+			$("#login-status").text(json['username']);
+			$("#logout-link").css({'visibility':'visible'});
+			$loginDialog.dialog('close');
+			$loginDialog = null;
+		    }
+		};
+		
+		$.ajax({
+  		    url: url,
+  		    dataType: 'json',
+  		    data: fieldsData,
+  		    success: callback,
+		    type: 'POST'
+		});
+            });
 	});
+	event.preventDefault();
+    });
 });
+
